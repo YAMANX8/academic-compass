6bfad4e4853933901b29ac8aac7d824d988050de
yamanx8 <yamanjazzar274@gmail.com>
2023-08-16 14:52:28 +0300

    initial the frontend

---
+++ frontend/src/redux/store.js
@@ -0,0 +1,9 @@
+import { configureStore } from '@reduxjs/toolkit';
+
+//here we import the slices from the feartures folder
+// import playerReducer from './features/playerSlice';
+
+export const store = configureStore({
+  reducer: {
+  },
+});
