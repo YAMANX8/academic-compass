1d7a1762ae46a534ca869b23c2852723accfe105
yamanx8 <yamanjazzar274@gmail.com>
2023-08-18 00:10:37 +0300

    adding the colors and the font family

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,jsx}'],
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#253AD4",
+        light: "#EEEFFC",
+        dark: "#070B27",
+        secondary: {
+          DEFAULT: "#E0E7F7",
+          dark: "#0D1832",
+        },
+        accent: {
+          DEFAULT: "#6A1EAD",
+          dark: "#B67CE8",
+        },
+      },
+    },
+    fontFamily: {
+      poppins: ["Poppins", "sans-serif"],
+    },
   },
   plugins: [],
-}
-
+};
