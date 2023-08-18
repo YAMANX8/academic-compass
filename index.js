34e9038d60239115661fcc98187a46dba4aa2e42
Ammar22-star0 <105159319+Ammar22-star0@users.noreply.github.com>
2023-08-18 16:26:51 +0300

    add api to student register and login

---
+++ index.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const app = express();
+
+
+//midleware
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+
+//ROUTES//
+// Register and login routes
+
+app.use("/auth",require('./Routes/Students/jwtAuth'));
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`server is up ans listening on port ${port}`);
+});
