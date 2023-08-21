3619a2464476b564e631f1a34b5cb1285acd01a9
yamanx8 <yamanjazzar274@gmail.com>
2023-08-21 12:14:29 +0300

    home-hero-desktop

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,43 +1,35 @@
-import React from 'react'
+import React from "react";
 import { FaRegMap as Map } from "react-icons/fa";
-import Hello from "../assets/images/hero.svg";
+import Hero from "../assets/images/hero.svg";
+import { Link } from "react-router-dom";
+
 function Home() {
+  const heading = "text-[3rem] font-semibold tracking-tight leading-[125%]";
+  const text = "";
   return (
-    <h1>home</h1>
-    
-//     <div className='w-[1440px] h-[795px] '>
-      
-      
-//       <div className=" text-5xl  font-semibold">
-//   <h1 className="ml-[120px] mr-[20px] mb-[22px] mt-[200px] ">
-//     <span className='block text-primary ' >A Roadmap-Driven</span>
-//     <span className='block' >Online Education</span>
-//     <span >Platform</span>
-//   </h1>
-// </div>
+    <div className="w-full bg-light mt-[100px] px-[120px] py-[48px] flex gap-4 justify-between items-center">
+      {/* ************************************************************ */}
+      <div className="flex flex-col gap-5 items-start">
+        <h1 className={`${heading} w-[488px]`}>
+          <span className="text-primary ">A Roadmap-Driven</span> Online Education Platform
+        </h1>
 
-// <div className=" text-3xl font-semibold	">
-//   <p className="ml-[120px] mr-[20px] mb-[22px] mt-[22px] ">
-//     <span className='block' >Unlocking Knowledge and</span>
-//     <span className='block' >Success Through Guided </span>
-//     <span >Learning Paths</span>
-//   </p>
-// </div>
+        <p className="text-[32px] font-medium leading-[125%] w-[488px]">
+          Unlocking Knowledge and Success Through Guided Learning Paths
+        </p>
+        {/* w-[262px] h-[44px]  bg-primary text-2xl ml-[120px] p-[6px] mr-[256px] mb-[207px] mt-[1px] font-medium items-center */}
+        <Link to="/roadmaps" className="flex gap-[10px] items-center font-semibold px-[20px] py-[10px] bg-primary text-light rounded-[5px]">
+          <Map className="text-[25px]"/>
+          Explore Our Roadmaps
+        </Link>
+      </div>
 
-//         <div className=' w-[262px] h-[44px]  bg-primary text-2xl ml-[120px] p-[6px] mr-[256px] mb-[207px] mt-[1px] font-medium items-center'>  
-//        <button  className=""> 
-//         Explore Our Roadmaps
-//         <Map className='mt-[-25px] pl-[px]'/>  
-//          </button>
-//          </div>
-  
-//          <div className='w-[696px] h-[696px] '>
-//            <img src={Hello} className='  ml-[20px] mt-[43px] mb-[60px] mr-[120px]' />
-//            </div>
-    
-//     </div>
-    
-  )
+      {/* ******************************************************** */}
+      <div className="w-[696px] h-[696px] ">
+        <img src={Hero} />
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
