fd6f1420e8914b3b67d4ce01defaf891390ee3e2
Ammar <esrawiaammar22@gmail.com>
2023-08-27 13:22:36 +0300

    settingApi is Updated

--- backend/Routes/Students/dashboardStudent/settings/security.js
+++ backend/Routes/Students/dashboardStudent/settings/security.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const pool = require("../../../../Database/db");
 const authorization = require("../../../../middleware/authorization.js");
-
+const bcrypt = require("bcrypt");
 
 // Change student password
 router.put('/', authorization , async (req, res) => {
@@ -16,19 +16,29 @@ router.put('/', authorization , async (req, res) => {
         if (rows.length === 0) {
             return res.status(404).json({ message: 'Student not found' });
         }
-
+        // todo here we need to compare bcrypt password
         const storedPassword = rows[0].password;
+
         
-        if (storedPassword !== currentPassword) {
-            return res.status(401).json({ message: 'Invalid current password' });
-        }
+         const validPassword = await bcrypt.compare(
+           currentPassword,
+           storedPassword
+         );
+         if (!validPassword) {
+           return res.status(401).json({ message: "Invalid current password" });
+         }
 
         if (newPassword !== verifyNewPassword) {
             return res.status(400).json({ message: 'New passwords do not match' });
         }
 
+        //* bcrypt new password
+        const saltRound = 10;
+        const newSalt = await bcrypt.genSalt(saltRound);
+        const newBcryptPassword = await bcrypt.hash(newPassword, newSalt);
+
         const updateQuery = 'UPDATE student SET password = $1 WHERE student_id = $2';
-        await pool.query(updateQuery, [newPassword, student_id]);
+        await pool.query(updateQuery, [newBcryptPassword, student_id]);
 
         return res.status(200).json({ message: 'Password updated successfully' });
     } catch (error) {
--- backend/Utils/dashboard/countDashboardS.js
+++ backend/Utils/dashboard/countDashboardS.js
@@ -43,7 +43,6 @@ const GetTotalPoit = async (student_id) => {
   }
 };
 
-
 module.exports = {
   GetCoursesNumberInfo,
   GetTotalPoit,
