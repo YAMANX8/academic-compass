0b0a51c2fcd0ccfc3abd15d5493c3e71265e1dc3
yamanx8 <yamanjazzar274@gmail.com>
2023-09-05 15:09:05 +0300

    change password functionality is added

--- frontend/src/components/setting/Security.jsx
+++ frontend/src/components/setting/Security.jsx
@@ -1,19 +1,61 @@
+import { useState } from "react";
 import { LiaSaveSolid as Solid } from "react-icons/lia";
+import axios from "../../apis/axios";
+import toast, { Toaster } from "react-hot-toast";
 //styles
 const inputStyle =
-"p-[10px] rounded-[5px] bg-secondary border border-dark/50 dark:border-light/50 text-[20px]";
+  "p-[10px] rounded-[5px] bg-secondary border border-dark/50 dark:border-light/50 text-[20px]";
+
+const SECURITY_URL = "/studentDashboard/change-password";
 
 const Security = () => {
-  return <form className="p-8 flex flex-col gap-8 flex-1">
-  <h3 className="text-[32px] tracking-tight font-semibold">
-      Security 
-  </h3>
-  <div>
-      <h4 className="text-[22px] tracking-tight font-semibold mb-4">
-      Password
-      </h4>
-      <div className="grid gap-4 grid-cols-2">
-          <div className="grid gap-2">
+  const [currentPassword, setCurrentPassword] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+  const [verifyNewPassword, setVerifyNewPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await axios.put(
+        SECURITY_URL,
+        JSON.stringify({ currentPassword, newPassword, verifyNewPassword }),
+        {
+          headers: {
+            token: localStorage.token,
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      toast.success(res.data.message);
+    } catch (err) {
+      if (!err?.response) {
+        toast.error("No Server Response");
+      } else if (err.response?.status === 401) {
+        toast.error(err.response.data.message);
+      } else if (err.response?.status === 400) {
+        toast.error(err.response.data.message);
+      } else if (err.response?.status === 404) {
+        toast.error(err.response.data.message);
+      } else {
+        toast.error("Login Failed");
+      }
+    }
+  };
+  console.log(currentPassword);
+  return (
+    <>
+      <div>
+        <Toaster />
+      </div>
+      <form className="p-8 flex flex-col gap-8 flex-1" onSubmit={handleSubmit}>
+        <h3 className="text-[32px] tracking-tight font-semibold">Security</h3>
+        <div>
+          <h4 className="text-[22px] tracking-tight font-semibold mb-4">
+            Password
+          </h4>
+          <div className="grid gap-4 grid-cols-2">
+            <div className="grid gap-2">
               <label
                 htmlFor="country"
                 className="font-medium text-[22px] leading-l"
@@ -25,11 +67,13 @@ const Security = () => {
                 className={`${inputStyle}`}
                 type="password"
                 placeholder="**********"
+                value={currentPassword}
+                onChange={(e) => setCurrentPassword(e.target.value)}
               />
-          </div>
-          <div className="grid gap-2">
-              <label 
-                htmlFor="newPassword" 
+            </div>
+            <div className="grid gap-2">
+              <label
+                htmlFor="newPassword"
                 className="font-medium text-[22px] leading-l"
               >
                 New password
@@ -39,32 +83,37 @@ const Security = () => {
                 className={`${inputStyle}`}
                 type="password"
                 placeholder="********"
+                value={newPassword}
+                onChange={(e) => setNewPassword(e.target.value)}
               />
-          </div>
-          <div className="grid gap-2 col-span-1 ">
-              <label 
-                htmlFor="verifyNewPassword" 
+            </div>
+            <div className="grid gap-2 col-span-1 ">
+              <label
+                htmlFor="verifyNewPassword"
                 className="font-medium text-[22px] leading-l"
               >
-                Verify new password 
+                Verify new password
               </label>
               <input
                 id="verifyNewPassword"
                 className={`${inputStyle}`}
                 type="password"
                 placeholder="********"
+                value={verifyNewPassword}
+                onChange={(e) => setVerifyNewPassword(e.target.value)}
               />
+            </div>
           </div>
-      </div>
-  </div>
-  <button
-    type="submit"
-    className="self-end flex justify-center items-center gap-[10px] px-[20px] py-[10px] font-semibold rounded-[5px] text-light bg-gradient-to-r from-primary to-accent"
-  >
-    Save Changes <Solid className="text-[25px]" />
-  </button>
-</form>
-;
+        </div>
+        <button
+          type="submit"
+          className="self-end flex justify-center items-center gap-[10px] px-[20px] py-[10px] font-semibold rounded-[5px] text-light bg-gradient-to-r from-primary to-accent"
+        >
+          Save Changes <Solid className="text-[25px]" />
+        </button>
+      </form>
+    </>
+  );
 };
 
 export default Security;
