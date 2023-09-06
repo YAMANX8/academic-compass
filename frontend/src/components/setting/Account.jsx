36d3ec0d4dc9a62f5e29f91e4e9fd1cbc44d999c
yamanx8 <yamanjazzar274@gmail.com>
2023-09-06 20:13:16 +0300

    fixing settings page and its components & refactor

--- frontend/src/components/setting/Account.jsx
+++ frontend/src/components/setting/Account.jsx
@@ -4,173 +4,172 @@ import toast, { Toaster } from "react-hot-toast";
 import { LiaSaveSolid as Solid } from "react-icons/lia";
 
 //styles
-const inputStyle = "p-[10px] rounded-[5px] bg-secondary border border-dark/50 dark:border-light/50 text-[20px]";
+const inputStyle =
+  "p-[10px] rounded-[5px] bg-light dark:bg-dark text-dark dark:text-light border border-dark/50 dark:border-light/50 text-[20px] transition-all duration-1000 ease-in-out-back";
+const labelStyle = "font-medium text-[22px] leading-l";
+// const GET_INFO_URL = "/studentDashboard/update-account/getInfo";
 
-const GET_INFO_URL = "/studentDashboard/update-account";
+const Account = ({
+  firstName,
+  lastName,
+  education,
+  email,
+  bio,
+  birthDate,
+  handleChange,
+}) => {
+  // const [firstName, setFirstName] = useState('');
+  // const [lastName, setLastName] = useState('');
+  // const [education, setEducation] = useState('');
+  // const [email, setEmail] = useState('');
+  // const [bio, setBio] = useState('');
+  // const [birthDate, setBirthDate] = useState('');
 
-const Account = () => {
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [education, setEducation] = useState('');
-  const [email, setEmail] = useState('');
-  const [bio, setBio] = useState('');
-  const [birthDate, setBirthDate] = useState('');
+  // useEffect(() => {
+  //   const fetchStudentInfo = async () => {
+  //     try {
+  //       const response = await axios.get(
+  //         GET_INFO_URL ,
+  //         JSON.stringify(
+  //           { first_name : firstName, last_name:lastName, email, education, birth_date :birthDate, bio }),
+  //       {
+  //         headers: {
+  //           token: localStorage.token,
+  //           "Content-Type": "application/json",
+  //         },
+  //       });
+  //       console.log(response.data);
 
-  useEffect(() => {
-    const fetchStudentInfo = async () => {
-      try {
-        const response = await axios.get(
-          GET_INFO_URL ,
-          JSON.stringify(
-            { first_name : firstName, last_name:lastName, email, education, birth_date :birthDate, bio }),
-        {
-          headers: {
-            token: localStorage.token,
-            "Content-Type": "application/json",
-          },
-        });
-        console.log(response.data);
+  //       if (response.data && response.data.Data && response.data.Data.data && response.data.Data.data.length) {
+  //         const studentData = response.data.Data.data[0];
+  //         setFirstName(studentData.first_name);
+  //         setLastName(studentData.last_name);
+  //         setEmail(studentData.email);
+  //         setEducation(studentData.education);
+  //         setBio(studentData.bio);
+  //         setBirthDate(studentData.birth_date);  console.log(response.data);
+  //       }
+  //     } catch (error) {
+  //       toast.error("Failed to fetch student information.");
+  //     }
+  //   };
 
-        if (response.data && response.data.Data && response.data.Data.data && response.data.Data.data.length) {
-          const studentData = response.data.Data.data[0];
-          setFirstName(studentData.first_name);
-          setLastName(studentData.last_name);
-          setEmail(studentData.email);
-          setEducation(studentData.education);
-          setBio(studentData.bio);
-          setBirthDate(studentData.birth_date);  console.log(response.data);
-        }
-      } catch (error) {
-        toast.error("Failed to fetch student information.");
-      }
-    };
+  //   fetchStudentInfo();
+  // }, []);
 
-    fetchStudentInfo();
-  }, []);
-
-
-  return <form className="p-8 flex flex-col gap-8 flex-1">
-  <h3 className="text-[32px] tracking-tight font-semibold">
-     Account 
-  </h3>
-  <div>
-      <h4 className="text-[22px] tracking-tight font-semibold mb-4">
-      Profile
-      </h4>
-      <div className="grid gap-4 grid-cols-2">
+  return (
+    <>
+      <h3 className="text-[32px] tracking-tight font-semibold">Account</h3>
+      <div>
+        <h4 className="text-[22px] tracking-tight font-semibold mb-4">
+          Profile
+        </h4>
+        <div className="grid gap-4 grid-cols-2">
+          <div className="grid gap-2">
+            <label htmlFor="firstName" className={`${labelStyle}`}>
+              First name
+            </label>
+            <input
+              id="firstName"
+              name="firstName"
+              className={`${inputStyle}`}
+              type="text"
+              placeholder="Jone"
+              value={firstName}
+              onChange={handleChange}
+            />
+          </div>
 
           <div className="grid gap-2">
-              <label
-                htmlFor="firstName"
-                className="font-medium text-[22px] leading-l"
-              >
-                First name
-              </label>
-              <input
-                id="firstName"
-                className={`${inputStyle}`}
-                type="text"
-                placeholder="Ahmad"
-                value={firstName}
-                onChange={(e) => setFirstName(e.target.value)}
-              />
-          </div>
-
-          <div className="grid gap-2">
-              <label 
-                htmlFor="lastName" 
-                className="font-medium text-[22px] leading-l"
-              >
-                Last Name
-              </label>
-              <input
-                id="lastName"
-                className={`${inputStyle}`}
-                type="text"
-                placeholder="omer"
-                value={lastName}
-                onChange={(e) => setLastName(e.target.value)}
-              />
-          </div>
-
-          <div className="grid gap-2 col-span-1 ">
-              <label 
-                htmlFor="education" 
-                className="font-medium text-[22px] leading-l"
-              >
-                Education
-              </label>
-              <input
-                id="education"
-                className={`${inputStyle}`}
-                type="text"
-                placeholder="BC in software engineering"
-                value={education}
-                onChange={(e) => setEducation(e.target.value)}
-              />
-          </div>
-
-          <div className="grid gap-2 col-span-1 ">
-              <label 
-                htmlFor="email" 
-                className="font-medium text-[22px] leading-l"
-              >
-                Email
-              </label>
-              <input
-                id="email"
-                className={`${inputStyle}`}
-                type="email"
-                placeholder="something@anything.com"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-          </div>
-
-          <div className="grid gap-2 col-span-1 ">
-              <label 
-                htmlFor="bio" 
-                className="font-medium text-[22px] leading-l"
-              >
-                Bio
-              </label>
-              <input
-                id="bio"
-                className={`${inputStyle}`}
-                type="text"
-                placeholder="A patinate software engineer stude....."
-                value={bio}
-                onChange={(e) => setBio(e.target.value)}
+            <label
+              htmlFor="lastName"
+              className={`${labelStyle}`}
+            >
+              Last Name
+            </label>
+            <input
+              id="lastName"
+              name="lastName"
+              className={`${inputStyle}`}
+              type="text"
+              placeholder="Doe"
+              value={lastName}
+              onChange={handleChange}
             />
           </div>
 
           <div className="grid gap-2 col-span-1 ">
-              <label 
-                htmlFor="birthDate" 
-                className="font-medium text-[22px] leading-l"
-              >
-                Birth of Date
-              </label>
-              <input
-                id="birthDate"
-                className={`${inputStyle}`}
-                type="date"
-                placeholder="22-8-2023"
-                value={birthDate}
-                onChange={(e) => setBirthDate(e.target.value)}
-              />
+            <label
+              htmlFor="education"
+              className={`${labelStyle}`}
+            >
+              Education
+            </label>
+            <input
+              id="education"
+              name="education"
+              className={`${inputStyle}`}
+              type="text"
+              placeholder="BC in software engineering"
+              value={education}
+              onChange={handleChange}
+            />
           </div>
 
-          
+          <div className="grid gap-2 col-span-1 ">
+            <label
+              htmlFor="email"
+              className={`${labelStyle}`}
+            >
+              Email
+            </label>
+            <input
+              id="email"
+              name="email"
+              className={`${inputStyle}`}
+              type="email"
+              placeholder="something@anything.com"
+              value={email}
+              onChange={handleChange}
+            />
+          </div>
+
+          <div className="grid gap-2 col-span-1 ">
+            <label htmlFor="bio" className={`${labelStyle}`}>
+              Bio
+            </label>
+            <input
+              id="bio"
+              name="bio"
+              className={`${inputStyle}`}
+              type="text"
+              placeholder="A patinate software engineer student"
+              value={bio}
+              onChange={handleChange}
+            />
+          </div>
+
+          <div className="grid gap-2 col-span-1 ">
+            <label
+              htmlFor="birthDate"
+              className={`${labelStyle}`}
+            >
+              Birth of Date
+            </label>
+            <input
+              id="birthDate"
+              name="birthDate"
+              className={`${inputStyle} dark:[color-scheme:dark]`}
+              type="date"
+              placeholder="22-8-2023"
+              value={birthDate}
+              onChange={handleChange}
+            />
+          </div>
+        </div>
       </div>
-  </div>
-  <button
-    type="submit"
-    className="self-end flex justify-center items-center gap-[10px] px-[20px] py-[10px] font-semibold rounded-[5px] text-light bg-gradient-to-r from-primary to-accent"
-  >
-    Save Changes <Solid className="text-[25px]" />
-  </button>
-</form>;
+    </>
+  );
 };
 
 export default Account;
--- frontend/src/components/setting/General.jsx
+++ frontend/src/components/setting/General.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { LiaSaveSolid as Solid } from "react-icons/lia";
 import { BsCloudUpload as Upload } from "react-icons/bs";
 import axios from "../../apis/axios";
@@ -6,121 +6,153 @@ import toast, { Toaster } from "react-hot-toast";
 import Profile from "../../assets/images/frontend.svg";
 import { Button } from "../index";
 
-const SECURITY_URL = "/setting";
+// const GENERAL_URL = "/setting/getInfo";
+// const UPDATE_GENERAL_URL = "/setting/addGeneralInfo";
 
-const General = () => {
-  const [country, setCountry] = useState("");
-  const [city, setCity] = useState("");
-  const [imagePath, setImagePath] = useState(Profile);
+const General = ({
+  firstName,
+  lastName,
+  country,
+  city,
+  image,
+  handleChange,
+}) => {
+  const photo = useRef();
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       const response = await axios.get(GENERAL_URL, {
+  //         headers: {
+  //           token: localStorage.token,
+  //           "Content-Type": "application/json",
+  //         },
+  //       });
+  //       const studentData = response.data.Data.data[0];
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get("/getInfo", {
-          headers: {
-            token: localStorage.token,
-          },
-        });
-        const studentData = response.data.Data.data[0];
-        setCountry(studentData.country);
-        setCity(studentData.city);
-        setImagePath(studentData.image_path);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    };
+  //       setCountry(studentData.country ? studentData.country : "");
+  //       setCity(studentData.city ? studentData.city : "");
+  //     } catch (error) {
+  //       console.error("Error fetching data:", error);
+  //     }
+  //   };
 
-    fetchData();
-  }, []);
+  //   fetchData();
+  // }, []);
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault();
 
-    try {
-      const res = await axios.put(
-        SECURITY_URL,
-        JSON.stringify({ country, city }),
-        {
-          headers: {
-            token: localStorage.token,
-            "Content-Type": "application/json",
-          },
-        }
-      );
-      toast.success(res.data.message);
-    } catch (err) {
-      if (!err?.response) {
-        toast.error("No Server Response");
-      } else {
-        toast.error("Login Failed");
-      }
-    }
-  };
+  //   try {
+  //     const res = await axios.put(
+  //       UPDATE_GENERAL_URL,
+  //       JSON.stringify({ country, city }),
+  //       {
+  //         headers: {
+  //           token: localStorage.token,
+  //           "Content-Type": "application/json",
+  //         },
+  //       }
+  //     );
+  //     toast.success(res.data.message);
+  //   } catch (err) {
+  //     if (!err?.response) {
+  //       toast.error("No Server Response");
+  //     } else {
+  //       toast.error("Login Failed");
+  //     }
+  //   }
+  // };
 
   //styles
   const inputStyle =
-    "p-[10px] rounded-[5px] bg-secondary border border-dark/50 dark:border-light/50 text-[20px]";
+    "p-[10px] rounded-[5px] bg-light dark:bg-dark text-dark dark:text-light border border-dark/50 dark:border-light/50 text-[20px] transition-all duration-1000 ease-in-out-back";
 
   return (
-    <form className="p-8 flex flex-col gap-8 flex-1 " onSubmit={handleSubmit}>
-      <h3 className="text-[32px] tracking-tight font-semibold ">General Information</h3>
+    <>
+      <h3 className="text-[32px] tracking-tight font-semibold ">
+        General Information
+      </h3>
 
       <div>
-        <h4 className="text-[22px] tracking-tight font-semibold mb-4">Profile Picture Upload</h4>
+        <h4 className="text-[22px] tracking-tight font-semibold mb-4">
+          Profile Picture Upload
+        </h4>
         <div className="flex gap-8">
-          <div>
-            <img className="w-[162px] aspect-square" src={imagePath} alt="Profile" />
+          <div className=" w-[162px] aspect-square flex justify-center items-center overflow-hidden rounded-full bg-primary text-light text-[36px] font-semibold">
+            {image ? (
+              <img className="object-cover" src={image} alt="Profile" />
+            ) : (
+              <p>
+                {firstName.charAt(0)} {lastName.charAt(0)}
+              </p>
+            )}
           </div>
           <div className="flex flex-col justify-between tracking-tight">
             <div>
-              <p className="font-medium text-[20px]">Jone Doe</p>
-              <p className="pt-[16px] text-primary">{`${city}, ${country}`}</p>
+              <p className="font-medium text-[20px]">{`${firstName} ${lastName}`}</p>
+              <p className="pt-[16px] text-dark/50 dark:text-light/50">
+                {(city || country) && `${city}, ${country}`}
+              </p>
             </div>
-            <div>
-              <Button>
-                Upload New Photo <Upload className="text-[25px]" />
-              </Button>
-            </div>
+            <button
+              type="button"
+              onClick={() => photo.current.click()}
+              className="flex justify-center items-center gap-[10px] px-[20px] py-[10px] font-semibold rounded-[5px] text-light bg-gradient-to-r from-primary to-accent"
+            >
+              <input
+                type="file"
+                name="image"
+                onChange={handleChange}
+                hidden
+                ref={photo}
+              />
+              Upload New Photo
+              <Upload />
+            </button>
           </div>
         </div>
       </div>
 
       <div>
-        <h4 className="text-[22px] tracking-tight font-semibold mb-4">Address</h4>
+        <h4 className="text-[22px] tracking-tight font-semibold mb-4">
+          Address
+        </h4>
         <div className="flex gap-4 justify-between">
           <div className="grid gap-2 flex-1">
-            <label htmlFor="country" className="font-medium text-[22px] leading-l">Country</label>
+            <label
+              htmlFor="country"
+              className="font-medium text-[22px] leading-l"
+            >
+              Country
+            </label>
             <input
               id="country"
+              name="country"
               className={`${inputStyle}`}
               type="text"
               placeholder="Syria"
               value={country}
-              onChange={(e) => setCountry(e.target.value)}
+              onChange={handleChange}
             />
           </div>
           <div className="grid gap-2 flex-1">
-            <label htmlFor="city" className="font-medium text-[22px] leading-l">City</label>
+            <label htmlFor="city" className="font-medium text-[22px] leading-l">
+              City
+            </label>
             <input
               id="city"
+              name="city"
               className={`${inputStyle}`}
               type="text"
               placeholder="Damascus"
               value={city}
-              onChange={(e) => setCity(e.target.value)}
+              onChange={handleChange}
             />
           </div>
         </div>
       </div>
-
-      <button
-        type="submit"
-        className="self-end flex justify-center items-center gap-[10px] px-[20px] py-[10px] font-semibold rounded-[5px] text-light bg-gradient-to-r from-primary to-accent"
-      >
-        Save Changes <Solid className="text-[25px]" />
-      </button>
-    </form>
+    </>
   );
-}
+};
 
 export default General;
