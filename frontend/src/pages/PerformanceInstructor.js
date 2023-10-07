835efdad091d88fb94c4bd6ea456ace2de7f3f28
YAMANX8 <yamanjazzar274@gmail.com>
2023-10-07 06:50:57 +0300

    fixing instructor dashboard && alot of refactors ☻

--- frontend/src/pages/PerformanceInstructor.js
+++ frontend/src/pages/PerformanceInstructor.js
@@ -1,27 +1,22 @@
 export const PerformanceInstructor = [
-    {
-      id: 1,
-      count:10,
-      title: "Total Enrollments",
-      color: "#253AD4",
-    },
-    {
-      id: 2,
-      count:20,
-      title: "Total Reviews",
-      color: "#25D42C",
-    },
-    {
-      id: 3,
-      count:30,
-      title: "Total Courses",
-      color: "#6A1EAD",
-    },
-    {
-      id: 4,
-      count:40,
-      title: "Total Students",
-      color: "#B67CE8",
-    },
-  ];
-  
+  {
+    id: 1,
+
+    color: "#253AD4",
+  },
+  {
+    id: 2,
+
+    color: "#25D42C",
+  },
+  {
+    id: 3,
+
+    color: "#6A1EAD",
+  },
+  {
+    id: 4,
+
+    color: "#B67CE8",
+  },
+];
