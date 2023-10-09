ef63f00a532e0c8a56adf538f14abd2bdf857139
YAMANX8 <yamanjazzar274@gmail.com>
2023-10-09 07:31:30 +0300

    adding json structure

--- frontend/src/pages/instructor interfaces/course info pages/Enrollments.jsx
+++ frontend/src/pages/instructor interfaces/course info pages/Enrollments.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import { useState } from "react";
 
 const Enrollments = () => {
-  return (
-    <div>Enrollments</div>
-  )
-}
+  const [data, setData] = useState([
+    {
+      id: 1,
+      first_name: "Ahmed",
+      last_name: "Sadek",
+      picture: "image path",
+      enroll_date: "05-05-2023",
+      country: "Syria",
+    },
+    {
+      id: 2,
+      first_name: "Ahmed",
+      last_name: "Omar",
+      picture: "image path",
+      enroll_date: "05-05-2023",
+      country: "Syria",
+    },
+  ]);
+  return <div>Enrollments</div>;
+};
 
-export default Enrollments
+export default Enrollments;
--- frontend/src/pages/instructor interfaces/edit course pages/Content.jsx
+++ frontend/src/pages/instructor interfaces/edit course pages/Content.jsx
@@ -1,9 +1,93 @@
-import React from 'react'
+import { useState } from "react";
 
 const Content = () => {
-  return (
-    <div>Content</div>
-  )
-}
+  const [data, setData] = useState([
+    {
+      id: 1,
+      topicTitle: "string",
+      subTopics: [
+        {
+          id: 1,
+          title: "string",
+          items: [
+            {
+              id: 1,
+              title: "string",
+              order: "number",
+              type: "string",
+            },
+            {
+              id: 2,
+              title: "string",
+              order: "number",
+              type: "string",
+            },
+          ],
+        },
+        {
+          id: 2,
+          title: "string",
+          items: [
+            {
+              id: 1,
+              title: "string",
+              order: "number",
+              type: "string",
+            },
+            {
+              id: 2,
+              title: "string",
+              order: "number",
+              type: "string",
+            },
+          ],
+        },
+      ],
+    },
+    {
+      id: 1,
+      topicTitle: "string",
+      subTopics: [
+        {
+          id: 1,
+          title: "string",
+          items: [
+            {
+              id: 1,
+              title: "string",
+              order: "number",
+              type: "string",
+            },
+            {
+              id: 2,
+              title: "string",
+              order: "number",
+              type: "string",
+            },
+          ],
+        },
+        {
+          id: 2,
+          title: "string",
+          items: [
+            {
+              id: 1,
+              title: "string",
+              order: "number",
+              type: "string",
+            },
+            {
+              id: 2,
+              title: "string",
+              order: "number",
+              type: "string",
+            },
+          ],
+        },
+      ],
+    },
+  ]);
+  return <div>Content</div>;
+};
 
-export default Content
+export default Content;
