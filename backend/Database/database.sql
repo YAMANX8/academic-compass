ac2ee91d9b5ddf295981d286ef360e5189cd2bbf
Ammar <esrawiaammar22@gmail.com>
2023-10-14 14:43:46 +0300

    fix bug

--- backend/Database/database.sql
+++ backend/Database/database.sql
@@ -2547,7 +2547,46 @@ INSERT INTO Course_Lists (item_body,item_order,list_type,course_id)
       JOIN Levels l ON course.course_level =l.level_id
 
        WHERE course_id=12
-       
+       ---
+               SELECT course_id, course_title,course_thumnail, progress
+          FROM (
+            SELECT c.course_id, c.course_title,c.course_thumnail,
+            CASE
+              WHEN c.subtitle IS NOT NULL THEN 1 ELSE 0 END +
+            CASE
+              WHEN c.course_description IS NOT NULL THEN 1 ELSE 0 END +
+            CASE
+              WHEN c.course_level IS NOT NULL THEN 1 ELSE 0 END +
+            CASE
+              WHEN c.course_type IS NOT NULL THEN 1 ELSE 0 END +
+            CASE
+              WHEN c.course_thumnail IS NOT NULL THEN 1 ELSE 0 END +
+            CASE
+              WHEN COUNT(CASE WHEN lt.type_name = 'In this course you will learn the following' THEN cl.list_id END) >= 1 THEN 1 ELSE 0 END +
+            CASE
+              WHEN COUNT(CASE WHEN lt.type_name = 'Who this course is for:' THEN cl.list_id END) >= 1 THEN 1 ELSE 0 END +
+            CASE
+              WHEN COUNT(CASE WHEN lt.type_name = 'Requirements' THEN cl.list_id END) >= 1 THEN 1 ELSE 0 END +
+            CASE
+              WHEN COUNT(DISTINCT I.item_id) >= 3 THEN 1 ELSE 0 END +
+            CASE
+              WHEN (
+                COUNT(DISTINCT CASE WHEN v.item_id IS NOT NULL THEN I.item_id END) >= 1
+                AND COUNT(DISTINCT CASE WHEN a.item_id IS NOT NULL THEN I.item_id END) >= 1
+                AND COUNT(DISTINCT CASE WHEN q.item_id IS NOT NULL THEN I.item_id END) >= 1
+              ) THEN 1 ELSE 0 END AS progress
+          FROM Course c
+          LEFT JOIN Course_Lists cl ON c.course_id = cl.course_id
+          LEFT JOIN List_Type lt ON cl.list_type = lt.type_id
+          LEFT JOIN Items I ON c.course_id = I.course_id
+          LEFT JOIN Video v ON I.item_id = v.item_id
+          LEFT JOIN Article a ON I.item_id = a.item_id
+          LEFT JOIN Quiz q ON I.item_id = q.item_id
+          WHERE c.instructor_id = 3
+          GROUP BY c.course_id, c.course_title
+        ) AS subquery
+        WHERE progress < 10;
+    
 
 
 
