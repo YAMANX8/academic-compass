1c63ff49d8d85486b03967194764168eef8b7f53
Yaman_JS <yamanjazzar274@gmail.com>
2024-05-20 22:01:59 +0300

    build the completed courses markup & fix bugs

--- frontend/src/components/interface/card.jsx
+++ frontend/src/components/interface/card.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Card = ({ children, className = "", title }) => {
   return (
-    <div className={`rounded-lg bg-light px-4 py-6 shadow-lg ${className}`}>
+    <div className={`rounded-lg px-4 py-6 shadow-lg bg-white dark:bg-black ${className}`}>
       {title && <h3 className="mb-2 w-full">{title}</h3>}
       {children}
     </div>
--- frontend/src/pages/instructor/dashboard/completed-courses.jsx
+++ frontend/src/pages/instructor/dashboard/completed-courses.jsx
@@ -1,7 +1,81 @@
-import React from "react";
+import CourseCard from "./components/course-card";
+import { useState } from "react";
+import { Card, TextField } from "../../../components";
+import { Helmet } from "react-helmet-async";
+import Image from "../../../assets/images/Rectangle 63.png";
 
 const CompletedCourses = () => {
-  return <div>CompletedCourses</div>;
+  const [data, setData] = useState([
+    {
+      id: 1,
+      title:
+        "yaman ipsum dolor sit amet consectetur adipisicing elit. Hic, praesentium minima a eos eligendi officiis cumque ea, magni dolores consequuntur, possimus nobis iure repellendus facere harum incidunt doloremque officia culpa.",
+      subtitle:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, praesentium minima a eos eligendi officiis cumque ea, magni dolores consequuntur, possimus nobis iure repellendus facere harum incidunt doloremque officia culpa.",
+      image: Image,
+      type: true,
+      active: true,
+    },
+    {
+      id: 2,
+      title:
+        "ahmed ipsum dolor sit amet consectetur adipisicing elit. Hic, praesentium minima a eos eligendi officiis cumque ea, magni dolores consequuntur, possimus nobis iure repellendus facere harum incidunt doloremque officia culpa.",
+      subtitle:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, praesentium minima a eos eligendi officiis cumque ea, magni dolores consequuntur, possimus nobis iure repellendus facere harum incidunt doloremque officia culpa.",
+      image: Image,
+      type: true,
+      active: false,
+    },
+    {
+      id: 3,
+      title:
+        "ammar ipsum dolor sit amet consectetur adipisicing elit. Hic, praesentium minima a eos eligendi officiis cumque ea, magni dolores consequuntur, possimus nobis iure repellendus facere harum incidunt doloremque officia culpa.",
+      subtitle:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, praesentium minima a eos eligendi officiis cumque ea, magni dolores consequuntur, possimus nobis iure repellendus facere harum incidunt doloremque officia culpa.",
+      image: Image,
+      type: true,
+      active: true,
+    },
+  ]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const filteredData = data.filter((course) =>
+    course.title.toLowerCase().includes(searchTerm.toLowerCase()),
+  );
+  return (
+    <>
+      <Helmet>
+        <title>My Completed Courses</title>
+      </Helmet>
+      <div className="w-full space-y-4">
+        <h2>My Completed Courses</h2>
+        <Card className="flex flex-col gap-4">
+          <input
+            type="text"
+            placeholder="Search by course title..."
+            className="px-3 h-14 rounded-lg w-80 border border-gray-400/20 outline-none"
+            value={searchTerm}
+            onChange={handleSearchChange}
+          />{" "}
+          {filteredData.map((course) => (
+            <CourseCard
+              key={course.id}
+              id={course.id}
+              title={course.title}
+              subtitle={course.subtitle}
+              type={course.type}
+              active={course.active}
+              image={course.image}
+            />
+          ))}
+        </Card>
+      </div>
+    </>
+  );
 };
 
 export default CompletedCourses;
