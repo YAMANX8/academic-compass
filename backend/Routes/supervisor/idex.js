b73c7664d530fdc784619225ff9d3e97f0acccd5
Ammar <esrawiaammar22@gmail.com>
2024-06-29 20:34:37 +0300

    add api's for manage-assgined topics

--- backend/Routes/supervisor/idex.js
+++ backend/Routes/supervisor/idex.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const assigningRoadmaps = require('./dashboard/assigning-roadmap');
 const instructors = require('./dashboard/instructors');
 const instructorProfile = require('./dashboard/instructor-profile');
+const manageAssignedTopics = require('./dashboard/manage-assgined-topics');
 const register = require('./register/jwt-auth');
 
 
@@ -15,6 +16,13 @@ router.use('/AcademicCompass/dashboard/instructors', instructors);
 
 // dashboard: instructor-profile
 router.use('/AcademicCompass/dashboard/instructor-profile', instructorProfile);
+
+// dashboard: instructor-profile
+router.use(
+  '/AcademicCompass/dashboard/manage-assigned-topics',
+  manageAssignedTopics,
+);
+
 
 // register
 router.use('/AcademicCompass/auth3', register);
