f9104e14c9e6f1937b2bd23372fa4f13c258d212
Yaman <yamanjazzar274@gmail.com>
2025-07-17 14:24:49 +0300

    change origin to all

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ const createUploadDirectory = require('./middleware/create-upload-directory');
 const app = express();
 
 // midleware
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: true, credentials: true }));
 // app.use(helmet()); // Disable tracking
 app.use(express.json());
 app.use(morgan('dev'));
